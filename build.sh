32b62fdfd5d0ef4b9ee2fa80c2434a0e497169fd
Joseph Szymborski <joseph.szymborski@mail.mcgill.ca>
2017-06-09 16:30:19 -0400

    changed output path of build script, added HTML5

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-asciidoctor-pdf -a pdf-style=resources/themes/basic-theme.yml thesis/thesis.asc
+asciidoctor-pdf -o output/pdf/thesis.pdf -a pdf-style=resources/themes/basic-theme.yml thesis/thesis.asc
+asciidoctor -o output/html/thesis.html thesis/thesis.asc
