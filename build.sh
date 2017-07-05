0b9194ab0d69499a2e93352a5d6129f8d34e7637
Joseph Szymborski <joseph.szymborski@mail.mcgill.ca>
2017-07-04 20:42:33 -0400

    added stylesdir

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-asciidoctor-pdf -o output/pdf/thesis.pdf -a pdf-style=resources/themes/basic-theme.yml -a pdf-fontsdir=resources/themes/ thesis/thesis.asc
+asciidoctor-pdf -o output/pdf/thesis.pdf -a pdf-stylesdir=resources/themes -a pdf-style=basic-theme.yml -a pdf-fontsdir=resources/themes/ thesis/thesis.asc
 asciidoctor -o output/html/thesis.html thesis/thesis.asc
