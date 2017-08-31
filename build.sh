aeaca4301f73a699443656e1c91d416e6740a93d
Joseph Szymborski <joseph.szymborski@naj-p.com>
2017-08-31 15:41:49 -0400

    LaTeX Rewrite + some additions
    Rewrote thesis in LaTeX because it's a lot nicer now :)
    Added a section on automated cancer detection
    Added a Gabor section

--- build.sh
+++
@@ -1,3 +0,0 @@
-asciidoctor-pdf -r asciidoctor-mathematical -a mathematical-format=svg -o output/pdf/thesis.pdf -a imagesoutdir=../output/tmp -a pdf-stylesdir=resources/themes -a pdf-style=basic-theme.yml -a pdf-fontsdir=resources/themes/ thesis/thesis.asc
-rm -rf ./output/tmp
-asciidoctor -o output/html/thesis.html thesis/thesis.asc
